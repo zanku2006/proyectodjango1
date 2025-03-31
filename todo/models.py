dab7c5439ce625529d341108ca766f0b13dc13fd
zanku2006 <gomezlopezhernandaniel@gmail.com>
2025-03-31 13:05:58 -0500

    mi primer proyecto en github

---
+++ todo/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class  tarea(models.Model):
+    tarea-models.CharField(max_length-100)
+    def __str__(self):
+        return super().__str__(tarea)
+# Create your models here.
