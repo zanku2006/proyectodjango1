f504a61782e013907f21b776f034394005e854c0
zanku2006 <gomezlopezhernandaniel@gmail.com>
2025-03-31 13:28:20 -0500

    Actualización del proyecto con nuevas modificaciones

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-class  tarea(models.Model):
-    tarea-models.CharField(max_length-100)
+
+class Tarea(models.Model):  
+    tarea = models.CharField(max_length=100)  
+
     def __str__(self):
-        return super().__str__(tarea)
+        return self.tarea  
+
 # Create your models here.
